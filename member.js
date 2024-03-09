3123300aa40416e011f00603d529df59506f7942
Marco017 <156084118+Marco017@users.noreply.github.com>
2024-03-09 14:33:01 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+  var skills = ["html", "css", "js"];
+  console.log(this);
+  console.log(this.name);
+  console.log(this.age);
+  console.log(this.skills);
+  console.log(this.skills[0]);
+  console.log(this.skills[1]);
+  console.log(this.skills[2]);
+}
